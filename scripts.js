75731b791a5b44f99e9cb13b876d8f93f85df81e
Alatreonn <127147130+Alatreonn@users.noreply.github.com>
2023-10-11 13:47:25 +0300

    Implemented RESET button

--- scripts.js
+++ scripts.js
@@ -1,23 +1,38 @@
-const grid = document.querySelector(`div[class="padInnerContainer"]`);
+const grid = document.querySelector(`.padInnerContainer`);
 
 function gridMaker(cellSize) {
     grid.style.gridTemplateColumns = `repeat(${cellSize}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${cellSize}, 1fr)`;
     for (let i = 2; i <= cellSize * cellSize; i++) {
         let divGrid = document.createElement("div");
+
         divGrid.classList.add("padCell")
         grid.appendChild(divGrid);
     }
 }
 
 function colorBlack () {
-    let singleCell = document.querySelectorAll(`div[class="padCell"]`);
+    let singleCell = document.querySelectorAll(`.padCell`);
 
     singleCell.forEach((singleCell) => {
     singleCell.addEventListener("mouseover", function () {
         singleCell.classList.remove("padCell");
-        singleCell.classList.add("padCellBlk");});
-    })};
+        singleCell.classList.add("padCellBlk");
+        });
+    });
+}
 
+function resetGrid() {
+    let resetButton = document.querySelector(".padButtonReset");
+    resetButton.addEventListener("click", function () {
+        let singleBlkCell = document.querySelectorAll(`.padCellBlk`);
+        singleBlkCell.forEach((singleCell) => {
+            singleCell.classList.remove("padCellBlk");
+            singleCell.classList.add("padCell");
+        });
+    });
+}
+
+resetGrid();
 gridMaker(50);
 colorBlack();
