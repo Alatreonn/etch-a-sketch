f2d77730c9e1c6f778edb489d66b62128de7c5bf
Alatreonn <127147130+Alatreonn@users.noreply.github.com>
2023-10-17 15:56:12 +0300

    Implemented Rainbow

--- scripts.js
+++ scripts.js
@@ -1,4 +1,5 @@
 const grid = document.querySelector(`.padInnerContainer`);
+const toggleColorButton = document.querySelector(".padButtonRainbow");
 
 function gridMaker(cellSize) {
     grid.style.gridTemplateColumns = `repeat(${cellSize}, 1fr)`;
@@ -11,28 +12,57 @@ function gridMaker(cellSize) {
     }
 }
 
-function colorBlack () {
-    let singleCell = document.querySelectorAll(`.padCell`);
+let colorBlackEnabled = true;
+function toggleColors() {
+    if (colorBlackEnabled) {
+        let singleCell = document.querySelectorAll(`.padCell`);
 
-    singleCell.forEach((singleCell) => {
-    singleCell.addEventListener("mouseover", function () {
-        singleCell.classList.remove("padCell");
-        singleCell.classList.add("padCellBlk");
+        singleCell.forEach((singleCell) => {
+        singleCell.addEventListener("mouseover", function () {
+            singleCell.style.backgroundColor = "black";
+            singleCell.style.borderColor = "black";
+            });
         });
-    });
+        console.log("color is black");
+        toggleColorButton.style.backgroundColor = "white";
+    }
+    else {
+        let singleCell = document.querySelectorAll(".padCell");
+
+        singleCell.forEach((singleCell) => {
+        singleCell.addEventListener("mouseover", function () {
+            let colorR = colorRainbowFunc();
+            singleCell.style.backgroundColor = colorR;
+            singleCell.style.borderColor = colorR;
+            });
+        });
+        toggleColorButton.style.backgroundColor = "gray";
+        console.log("color is rainbow");
+    }
+    colorBlackEnabled = !colorBlackEnabled;
 }
+toggleColorButton.addEventListener("click", toggleColors);
 
 function resetGrid() {
     let resetButton = document.querySelector(".padButtonReset");
     resetButton.addEventListener("click", function () {
-        let singleBlkCell = document.querySelectorAll(`.padCellBlk`);
-        singleBlkCell.forEach((singleCell) => {
-            singleCell.classList.remove("padCellBlk");
-            singleCell.classList.add("padCell");
+        let singleCell = document.querySelectorAll(`.padCell`);
+        singleCell.forEach((singleCell) => {
+            singleCell.style.backgroundColor = "#a2a2a2";
+            singleCell.style.borderColor = "#adadad";
         });
     });
 }
 
+function colorRainbowFunc() {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
 resetGrid();
 gridMaker(50);
-colorBlack();
+toggleColors();
