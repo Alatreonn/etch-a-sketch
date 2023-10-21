baa5077bb0660d7a59d787ee972b35e84cd5ccb1
Alatreonn <127147130+Alatreonn@users.noreply.github.com>
2023-10-21 11:29:19 +0300

    Implemented grid resizing

--- scripts.js
+++ scripts.js
@@ -1,59 +1,71 @@
-const grid = document.querySelector(`.padInnerContainer`);
+const grid = document.querySelector(".padInnerContainer");
 const toggleColorButton = document.querySelector(".padButtonRainbow");
 
-function gridMaker(cellSize) {
-    grid.style.gridTemplateColumns = `repeat(${cellSize}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${cellSize}, 1fr)`;
-    for (let i = 2; i <= cellSize * cellSize; i++) {
+function createGrid(cellCreate) {
+    grid.style.gridTemplateColumns = `repeat(${cellCreate}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${cellCreate}, 1fr)`;
+    for (let i = 1; i <= cellCreate * cellCreate; i++) {
         let divGrid = document.createElement("div");
 
         divGrid.classList.add("padCell")
         grid.appendChild(divGrid);
+        }
+        //When resizing, hover-to-color doesn't work unless "rainbow" button is pressed, this fixes it, toggling it back and forth.
+        toggleColors()
     }
+
+function resizeGrid(cellResize) {
+    let divCells = document.querySelectorAll(".padCell");
+        if (cellResize >= 2 && cellResize <= 100) {
+        divCells.forEach((cellResize) => cellResize.remove());
+
+        createGrid(cellResize);
+        //When resizing, hover-to-color doesn't work unless "rainbow" button is pressed, this fixes it, toggling it back and forth.
+        toggleColors()
+        }
+    };
+
+function resetGrid() {
+    let resetButton = document.querySelector(".padButtonReset");
+    resetButton.addEventListener("click", function () {
+        let divCells = document.querySelectorAll(".padCell");
+        divCells.forEach((divCells) => {
+        divCells.style.backgroundColor = "#a2a2a2";
+        divCells.style.borderColor = "#adadad";
+        });
+    });
 }
 
-let colorBlackEnabled = true;
+let colorBlackEnabled = false;
 function toggleColors() {
     if (colorBlackEnabled) {
-        let singleCell = document.querySelectorAll(`.padCell`);
+        let divCells = document.querySelectorAll(`.padCell`);
 
-        singleCell.forEach((singleCell) => {
-        singleCell.addEventListener("mouseover", function () {
-            singleCell.style.backgroundColor = "black";
-            singleCell.style.borderColor = "black";
+        divCells.forEach((divCells) => {
+        divCells.addEventListener("mouseover", function () {
+            divCells.style.backgroundColor = "black";
+            divCells.style.borderColor = "black";
             });
         });
-        console.log("color is black");
         toggleColorButton.style.backgroundColor = "white";
     }
     else {
-        let singleCell = document.querySelectorAll(".padCell");
+        let divCells = document.querySelectorAll(".padCell");
 
-        singleCell.forEach((singleCell) => {
-        singleCell.addEventListener("mouseover", function () {
+        divCells.forEach((divCells) => {
+        divCells.addEventListener("mouseover", function () {
             let colorR = colorRainbowFunc();
-            singleCell.style.backgroundColor = colorR;
-            singleCell.style.borderColor = colorR;
+            divCells.style.backgroundColor = colorR;
+            divCells.style.borderColor = colorR;
             });
         });
         toggleColorButton.style.backgroundColor = "gray";
-        console.log("color is rainbow");
     }
     colorBlackEnabled = !colorBlackEnabled;
 }
 toggleColorButton.addEventListener("click", toggleColors);
 
-function resetGrid() {
-    let resetButton = document.querySelector(".padButtonReset");
-    resetButton.addEventListener("click", function () {
-        let singleCell = document.querySelectorAll(`.padCell`);
-        singleCell.forEach((singleCell) => {
-            singleCell.style.backgroundColor = "#a2a2a2";
-            singleCell.style.borderColor = "#adadad";
-        });
-    });
-}
-
+//Creates a random color by combining "#" with 6 other Hex chars and returns it.
 function colorRainbowFunc() {
     const letters = "0123456789ABCDEF";
     let color = "#";
@@ -63,6 +75,7 @@ function colorRainbowFunc() {
     return color;
 }
 
+createGrid(16);
 resetGrid();
-gridMaker(50);
+resizeGrid();
 toggleColors();
